f0ba51b80796e2556cbc69b2ad0302d91d394ca1
khikisb <89850965+khikisb@users.noreply.github.com>
2022-11-22 18:27:05 +0700

    Update Prediksi-Kesadaran-Pelecehan-Seksual-Anak.py

--- Prediksi-Kesadaran-Pelecehan-Seksual-Anak.py
+++ Prediksi-Kesadaran-Pelecehan-Seksual-Anak.py
@@ -66,6 +66,20 @@ st.set_page_config(
 st.title("")
 st.sidebar.success("Pilih Halaman Yang Ingin Anda Tuju.")
 
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
 
 st.title("Prediksi Kesadaran Pelecehan Seksual Anak")
 st.header('Jawablah Semua Pertanyaan Berikut :')
