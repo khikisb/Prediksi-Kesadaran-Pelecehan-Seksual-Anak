3f245d76d8fe7336d292df0206ea74e1906fa143
khikisb <89850965+khikisb@users.noreply.github.com>
2022-11-22 11:30:48 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# load the model from disk
+model = pickle.load(open('model_file.pkl', 'rb'))
+
+#Caching the model for faster loading
+@st.cache
+
+
+# Define the prediction function
+def predict(q1):
+    #Predicting the price of the carat
+    if q1 == 'Agree':
+        q1 = 1
+    elif cut == 'Disagree':
+        q1 = 0
+    
+    prediction = model.predict(pd.DataFrame([[q1]], columns=["Children are safe among family members such as grandparents, uncles, aunts, cousins"]))
+    return prediction
+
+st.set_page_config(
+    page_title="Prediksi Harga Berlian",
+    page_icon="👋",
+)
+
+st.title("")
+st.sidebar.success("Pilih Halaman Yang Ingin Anda Tuju.")
+
+
+st.title('Prediksi Harga Berlian')
+st.image('')
+st.header('Jawablah Semua Pertanyaan Berikut :')
+
+q1 = st.selectbox('"Children are safe among family members such as grandparents, uncles, aunts, cousins"', ['Aggree', 'Disagree'])
+
+if st.button('Prediksi Harga Belian'):
+    price = predict(q1)
+    st.success(f'Harga Berlian Tersebut adalah {price} USD')
