b707b8d6cafc0445f5539330796f5299defcc368
khikisb <89850965+khikisb@users.noreply.github.com>
2022-11-22 13:18:36 +0700

    Add files via upload

---
+++ pages/👨‍💼Tentang-Pengembang.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Tentang Pengembang",
+    page_icon="👋",
+)
+
+st.title("Halo Semuanya !👋")
+
+st.image('tentang-pengembang.png')
---
+++ pages/🤫Code-Project.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Code-Project",
+    page_icon="👋",
+)
+
+st.title("Halo Semuanya !👋")
+
+st.write('Kamu Bisa menemukan Source Code Project ini')
+
+link = '[Di GitHub](https://github.com/khikisb/Prediksi-Kesadaran-Pelecehan-Seksual-Anak)'
+st.markdown(link, unsafe_allow_html=True)
